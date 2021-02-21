ef48c3cbef850f793a602ecfcf525cbb1210d3b0
huansi <huansi.guo@dcmail.ca>
2021-02-21 18:27:24 -0500

    add show current template plugin, upload content to posts, styled header

--- themes/likeiotheme/single.php
+++ themes/likeiotheme/single.php
@@ -10,7 +10,7 @@
 get_header();
 ?>
 
-	<main id="primary" class="site-main">
+	<main id="primary" class="site-main grid-container">
 
 		<?php
 		while ( have_posts() ) :
@@ -26,9 +26,9 @@ get_header();
 			);
 
 			// If comments are open or we have at least one comment, load up the comment template.
-			if ( comments_open() || get_comments_number() ) :
-				comments_template();
-			endif;
+			// if ( comments_open() || get_comments_number() ) :
+			// 	comments_template();
+			// endif;
 
 		endwhile; // End of the loop.
 		?>
