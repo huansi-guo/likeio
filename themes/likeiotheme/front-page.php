90d82c28352dd4a72d44ef200f0fad7d60d4026a
huansi <huansi.guo@dcmail.ca>
2021-02-28 12:35:04 -0500

    create front page template

--- themes/likeiotheme/front-page.php
+++ themes/likeiotheme/front-page.php
@@ -1,8 +1,8 @@
 <?php
 /**
- * The template for displaying all pages
+ * The template for displaying front page
  *
- * This is the template that displays all pages by default.
+ * This is the template that displays front page by default.
  * Please note that this is the WordPress construct of pages
  * and that other 'pages' on your WordPress site may use a
  * different template.
