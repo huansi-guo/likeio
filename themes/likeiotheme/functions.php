ca0d8b9948a2161ac9807206a2c7dffb3ef30086
huansi <huansi.guo@dcmail.ca>
2021-02-28 13:44:27 -0500

    update the product image

--- themes/likeiotheme/functions.php
+++ themes/likeiotheme/functions.php
@@ -257,11 +257,11 @@ function likeiotheme_scripts() {
 
 
 	// Enqueue shop single style ********************************************
-	// wp_enqueue_style(
-	// 	'shopSingle-style',
-	// 	get_stylesheet_directory_uri() . '/assets/css/shopSingle.css',
-	// 	array()
-	// );
+	wp_enqueue_style(
+		'shopSingle-style',
+		get_stylesheet_directory_uri() . '/assets/css/shopSingle.css',
+		array()
+	);
 
 	// Enqueue what input js
 	wp_enqueue_script ( 'likeiotheme-what-input', get_template_directory_uri() .'/assets/js/vendor/what-input.js', array('jquery'), '6.5.1', true);
