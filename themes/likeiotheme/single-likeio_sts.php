b372e9afe736eaab8f7fcb2b2e7e98727168e382
huansi <huansi.guo@dcmail.ca>
2021-03-14 13:03:53 -0400

    add conditional tag in single custom post type to fit banner

--- themes/likeiotheme/single-likeio_sts.php
+++ themes/likeiotheme/single-likeio_sts.php
@@ -16,25 +16,32 @@ get_header();
 ?>
 
 	<div id="primary" class="singleSmartLife content-area grid-container">
-	
-		<main id="main" class="site-main grid-x smartingle">
+		<?php
+		while ( have_posts() ) :
+			if(has_post_thumbnail()):?>
+				<div class="post-thumbnail">
+					<?php the_post_thumbnail(); ?>
+				</div><!-- .post-thumbnail -->
+			<?php endif; ?>
+
+		<main id="main" class="site-main grid-x smartSingle">
 
 			<div class="cell large-1 medium-1"></div>
 
 			<div class="paddingInside cell large-10 medium-10 small-12">
-				<?php
-				while ( have_posts() ) :
-					the_post();
 
-					get_template_part( 'template-parts/content', 'page' );
+			<?php
+				the_post();
 
-					// If comments are open or we have at least one comment, load up the comment template.
-					// if ( comments_open() || get_comments_number() ) :
-					// 	comments_template();
-					// endif;
+				get_template_part( 'template-parts/content', 'page' );
 
-				endwhile; // End of the loop.
-				?>
+				// If comments are open or we have at least one comment, load up the comment template.
+				// if ( comments_open() || get_comments_number() ) :
+				// 	comments_template();
+				// endif;
+
+		endwhile; // End of the loop.
+		?>
 			</div>	
 
 			<div class="cell large-1 medium-1"></div>
